231654576b3a9bc43548fc626deee00276ce2f3c
prathma <prathma@192.168.1.3>
2020-01-02 11:58:47 +0530

    First project committed..

---
+++ MyFirstProjectInEclipse/src/com/kamath/prabhudas/MyFirstProgramInEclipse.java
@@ -0,0 +1,14 @@
+package com.kamath.prabhudas;
+
+import java.util.Scanner;
+
+public class MyFirstProgramInEclipse {
+
+	public static void main(String[] args) {
+		System.out.println("My First Program in Eclipse");
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println(s);
+		sc.close();
+	}
+}
