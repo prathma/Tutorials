c71fb79520acd53c5e4639bd8b424c18eae3688d
prathma <prathma@192.168.1.3>
2020-01-02 12:20:32 +0530

    Changed

--- MyFirstProjectInEclipse/src/com/kamath/prabhudas/MyFirstProgramInEclipse.java
+++ MyFirstProjectInEclipse/src/com/kamath/prabhudas/MyFirstProgramInEclipse.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MyFirstProgramInEclipse {
 
 	public static void main(String[] args) {
-		System.out.println("My First Program in Eclipse");
+		System.out.println("My First Program in Eclipse to be updated in GITHub");
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
 		System.out.println(s);
