9cc0947390ca93699131088c110016861a06a2c8
prathma <prathma@192.168.1.3>
2020-01-02 13:37:29 +0530

    Test

--- MyFirstProjectInEclipse/src/com/kamath/prabhudas/MyFirstProgramInEclipse.java
+++ MyFirstProjectInEclipse/src/com/kamath/prabhudas/MyFirstProgramInEclipse.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class MyFirstProgramInEclipse {
 
 	public static void main(String[] args) {
-		System.out.println("My First Program in Eclipse to be updated in GITHub");
+		System.out.println("My First Program  in Eclipse to be updated in GITHub to be Uploaded");
 		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		System.out.println(s);
+		while (true) {
+			String s=sc.nextLine();
+			System.out.println(s);
+			if (s.equals("Exit")) {
+				break;
+			}
+		}
 		sc.close();
 	}
 }
